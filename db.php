19e550f7be9ef95fe69a215c35f88363b0235e47
NGUYEN HUU TRUNG <trungnhvn@gmail.com>
2019-05-22 01:33:31 +0700

    commit

---
+++ db.php
@@ -0,0 +1,8 @@
+<?php
+// timezone
+date_default_timezone_set ( 'Asia/Ho_Chi_Minh' );
+// connection to the database
+define ( 'SERVER' , 'localhost' );
+define ( 'USER' , 'root' );
+define ( 'PASS' , '123456' );
+define ( 'DATABASE' , 'DemoPHP' );
